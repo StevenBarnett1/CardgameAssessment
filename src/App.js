8e3f76463f135a518b82fb0b8974625e49fe22c1
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-12-06 13:20:13 -0800

    added all, for some reason mapping cards isnt working, also need to add validation for buttons after dealing, ran out of time

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import CardGame from './Components/CardGame/CardGame';
 function App() {
   return(
-
     <CardGame/>
-
   )
 
 
--- src/Components/CardGame/CardGame.js
+++ src/Components/CardGame/CardGame.js
@@ -7,17 +7,37 @@ let CardGame = () => {
     const [numberOfPlayers,setNumberOfPlayers] = useState("")
     const [cardsPerPlayer,setCardsPerPlayer] = useState("")
     const [errors,setErrors] = useState([])
+    const [playerHands,setPlayerHands] = useState([])
 
     useEffect(()=>{
         setDeck(["Ac","Kc","Qc","Jc","10c","9c","8c","7c","6c","5c","4c","3c","2c","Ad","Kd","Qd","Jd","10d","9d","8d","7d","6d","5d","4d","3d","2d","As","Ks","Qs","Js","10s","9s","8s","7s","6s","5s","4s","3s","2s","Ah","Kh","Qh","Jh","10h","9h","8h","7h","6h","5h","4h","3h","2h",])
     },[])
 
     const dealCards = () => {
-
+        let player = 1
+        let playerHandsCopy = []
+        while(player <= numberOfPlayers){
+            let card = 1
+            let playerHand = []
+            while(card <= cardsPerPlayer){
+                playerHand.push(deck.pop())
+                card++
+            }
+            playerHandsCopy = [...playerHandsCopy,playerHand]
+            player++
+        }
+        console.log("NUM PLAYERS: ",numberOfPlayers)
+        console.log("CARDS PER PLAYER: ",cardsPerPlayer)
+        console.log("HERE: ",playerHandsCopy)
+        setPlayerHands(playerHandsCopy)
     }
 
+    useState(()=>{
+        console.log("PLAYER HANDS CHANGED", playerHands)
+    },playerHands)
+
     const shuffleDeck = () => {
-        let deckCopy = [...deck]
+        let deckCopy = ["Ac","Kc","Qc","Jc","10c","9c","8c","7c","6c","5c","4c","3c","2c","Ad","Kd","Qd","Jd","10d","9d","8d","7d","6d","5d","4d","3d","2d","As","Ks","Qs","Js","10s","9s","8s","7s","6s","5s","4s","3s","2s","Ah","Kh","Qh","Jh","10h","9h","8h","7h","6h","5h","4h","3h","2h",]
         let i,j,sub
         for (i = deckCopy.length - 1; i > 0; i--) {
             j = Math.floor(Math.random() * (i + 1));
@@ -26,6 +46,7 @@ let CardGame = () => {
             deckCopy[j] = sub;
         }
         setDeck(deckCopy)
+        console.log(deckCopy)
     }
 
     const submitForm = (e) => {
@@ -43,15 +64,20 @@ let CardGame = () => {
         }
         if(!currentErrors.length){
             togglePlayerOptions(false)
+            dealCards()
         }
         else setErrors(currentErrors)
 
+    }
+    console.log("PLAYER HANDS: ",playerHands)
+    if(playerHands.length){
+        console.log(playerHands.map(hand => hand.map(card => card)))
     }
 
     return (
         <>
             <h1>CardGame Page</h1>
-            <button onClick = {()=>shuffleDeck}>Shuffle Deck</button>
+            <button onClick = {()=>shuffleDeck()}>Shuffle Deck</button>
             <button onClick = {()=>togglePlayerOptions(!playerOptions)}>Deal Cards</button>
             <form onSubmit = {(e)=>submitForm(e)} style = {playerOptions ? {display:"block"} : {display:"none"}}>
                 <div id = "errors">{errors.map(error=>(
@@ -61,8 +87,16 @@ let CardGame = () => {
                 <input type = "Number" placeholder = "Number of Players" onChange = {e=>setNumberOfPlayers(Number(e.target.value))}></input>
                 <input type = "Number" placeholder = "Cards Per Player" onChange = {e=>setCardsPerPlayer(Number(e.target.value))}></input>
                 <input type = "submit"></input>
-
             </form>
+            <div>
+                {playerHands && playerHands.map(hand => {
+                    <div>
+                        {hand.map(card=>{
+                            <div>{card}</div>
+                        })}
+                    </div>
+                })}
+            </div>
         </>
     )
 
